3ee2d9ad61ba80d42467102704d4f9a61ac9d51d
Andrew Fyfe <enumera2000@yahoo.co.uk>
2014-08-16 14:43:12 +0100

    Temporary home page introduction with home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WifiJnLdn2014Psql::Application.routes.draw do
+  get "home/index"
+
    devise_scope :user do
     get "/login", to: "devise/sessions#new"
   end 
@@ -56,7 +58,7 @@ WifiJnLdn2014Psql::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
