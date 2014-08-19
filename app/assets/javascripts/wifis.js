ff6dc335bd78db023456f809ca014a8e4fa4ea8a
Tom <tj.walker@live.com>
2014-08-19 08:47:35 +0100

    wifi index accordian

---
+++ app/assets/javascripts/wifis.js
@@ -0,0 +1,19 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+//Wifi accordian code
+
+// $(document).ready(function(){
+
+//   var allPanels = $('.accordion > dd').hide();
+    
+//   $('.accordion > dt > a').click(function() {
+//     allPanels.slideUp();
+//     $(this).parent().next().slideDown();
+//     return false;
+//   });
+
+// })
+
+  
