12f7d953db157f3508d0789ec6f7318a1f339727
Andrew Fyfe <enumera2000@yahoo.co.uk>
2014-08-19 18:05:43 +0100

    changes on dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :user_type_id, :first, :last, :dob, :sex, :occupation, :position, :image, :role,
-  :phone, :uid, :provider
+  :phone, :uid, :provider, :group_ids
   # attr_accessible :title, :body
 
   belongs_to :user_type
